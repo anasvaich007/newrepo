144e072963a507b9f3a532f9c7cd8e477b0fa28b
Anastasia <nastyaaanufrieva@gmail.com>
2025-08-07 18:29:20 +0300

    Домашнее задание 1

---
+++ урок1/lesson_1_task_4.py
@@ -0,0 +1,11 @@
+def print_greeting():
+    print(
+        "Привет, мир! Ля ля ля, я -", first_name, last_name,
+        "у меня башка из картошки"
+    )
+
+
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+
+print_greeting()
